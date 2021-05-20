ddd848d5b4aed80d0c6fe570e58d49654f5312da
taka <shinegojoe@gmail.com>
2021-05-21 00:36:10 +0800

    add page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import logo from './logo.svg'
 import './App.css'
 import Main from './component/main'
+import Home from './component/home'
 
 
 function App() {
   
   return (
     <div className="App">
-      <Main></Main>
+      {/* <Main></Main> */}
+      <Home></Home>
     </div>
   );
 }
